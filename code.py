11c6e592ec88bc02e0cbb36fa5ca524f881ed29f
ALPHAMICKISNOOB <96507640+ALPHAMICKISNOOB@users.noreply.github.com>
2023-03-21 19:53:56 +0530

    Delete code.py

--- code.py
+++
@@ -1,7 +0,0 @@
-from discord_webhook import DiscordWebhook
-while True:
-    content = input("The message to send from your webhook : ")
-    webhook_url = 'Webhook_url'
-    #executes the Webhook
-    webhook = DiscordWebhook(url=webhook_url, content=content)
-    response = webhook.execute()
